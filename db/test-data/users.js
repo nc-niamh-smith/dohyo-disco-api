dd7fc2ff2f9e29398f7c0828f4a9aa75a6dd52ce
nc-niamh-smith <niamh.smith@northcoders.com>
2024-02-01 12:20:15 +0000

    add test data

---
+++ db/test-data/users.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {username: 'dinoscaup', stable_name: 'honshu'},
+    {username: 'sloorfin', stable_name: 'big heya'},
+    {username: 'huornchtu', stable_name: 'not horses'}
+]
