206d55b0bedf281cc28dc9327bc19cd8a877667e
nc-niamh-smith <niamh.smith@northcoders.com>
2024-02-02 09:45:04 +0000

    testing users table exists

--- db/__testing__/seed.test.js
+++ db/__testing__/seed.test.js
@@ -57,4 +57,54 @@ describe("seed", () => {
             expect(column.column_default).toBe(null);
         })
     });
+    describe("users table", () => {
+        test("users table exists", async () => {
+            const query = await db.query(
+                `SELECT EXISTS (
+                    SELECT FROM
+                    information_schema.tables
+                    WHERE
+                    table_name = 'users'
+                );`
+            )
+            const exists = query.rows[0].exists;
+            expect(exists).toBe(true);
+        });
+        test("users table has a username column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'users'
+                AND column_name = 'username';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("username");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("users table has a stable_name column as a FOREIGN KEY", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'users'
+                AND column_name = 'stable_name';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("stable_name");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("users table has a user_id column as a serial primary key", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type, column_default
+                FROM information_schema.columns
+                WHERE table_name = 'users'
+                AND column_name = 'user_id';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("user_id");
+            expect(column.data_type).toBe("integer");
+            expect(column.column_default).toBe(
+                "nextval('users_user_id_seq'::regclass)"
+            );
+        });
+    })
 });
--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,22 @@
 const db = require('./connection.js')
 
 const seed = async ({users, stables, rikishi}) => {
-    await db.query(`DROP TABLE IF EXISTS stables;`)
     await db.query(`DROP TABLE IF EXISTS users;`)
+    await db.query(`DROP TABLE IF EXISTS stables;`)
     await db.query(`DROP TABLE IF EXISTS sumos;`)
     await db.query(`DROP TABLE IF EXISTS stables_sumos_junc;`)
     //create stables
     await db.query(`CREATE TABLE stables (
-        stable_name VARCHAR(500),
+        stable_name VARCHAR(500) UNIQUE,
         stable_id SERIAL PRIMARY KEY,
         ranking INTEGER
     );`)
     //create users
-    // await db.query(`CREATE TABLE users (
-
-    // )`)
+    await db.query(`CREATE TABLE users (
+        username VARCHAR(50),
+        stable_name VARCHAR REFERENCES stables(stable_name) NOT NULL,
+        user_id SERIAL PRIMARY KEY NOT NULL
+    );`)
     //create sumos
     //create junction table (many to many)
     //populate stables
