2e6a6568fef317713810ae22f12fdf56fcd5f6bc
nc-niamh-smith <niamh.smith@northcoders.com>
2024-02-02 11:49:39 +0000

    testing rikishi and junction table

--- db/__testing__/seed.test.js
+++ db/__testing__/seed.test.js
@@ -81,7 +81,7 @@ describe("seed", () => {
             expect(column.column_name).toBe("username");
             expect(column.data_type).toBe("character varying");
         });
-        test("users table has a stable_name column as a FOREIGN KEY", async () => {
+        test("users table has a stable_name column as a reference to stables table", async () => {
             const query = await db.query(
                 `SELECT column_name, data_type
                 FROM information_schema.columns
@@ -107,4 +107,204 @@ describe("seed", () => {
             );
         });
     })
+    describe("rikishi table", () => {
+        test("rikishi table exists", async () => {
+            const query = await db.query(
+                `SELECT EXISTS (
+                    SELECT FROM
+                    information_schema.tables
+                    WHERE
+                    table_name = 'rikishi'
+                );`
+            )
+            const exists = query.rows[0].exists;
+            expect(exists).toBe(true);
+        });
+        test("rikishi table has a sumodb_id column as integer", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'sumodb_id';`
+            );
+            console.log(query)
+            const column = query.rows[0]
+            expect(column.column_name).toBe("sumodb_id");
+            expect(column.data_type).toBe("integer");
+        });
+        test("rikishi table has a nsk_id column as integer", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'nsk_id';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("nsk_id");
+            expect(column.data_type).toBe("integer");
+        });
+        test("rikishi table has a id column as a serial primary key", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type, column_default
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'id';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("id");
+            expect(column.data_type).toBe("integer");
+            expect(column.column_default).toBe(
+                "nextval('rikishi_id_seq'::regclass)"
+            );
+        });
+        test("rikishi table has a shikona_en column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'shikona_en';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("shikona_en");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("rikishi table has a shikona_jp column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'shikona_jp';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("shikona_jp");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("rikishi table has a current_rank column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'current_rank';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("current_rank");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("rikishi table has a heya column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'heya';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("heya");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("rikishi table has a birth_date column as DATE", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'birth_date';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("birth_date");
+            expect(column.data_type).toBe("date");
+        });
+        test("rikishi table has a shusshin column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'shusshin';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("shusshin");
+            expect(column.data_type).toBe("character varying");
+        });
+        test("rikishi table has a height column as integer", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'height';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("height");
+            expect(column.data_type).toBe("integer");
+        });
+        test("rikishi table has a weight column as integer", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'weight';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("weight");
+            expect(column.data_type).toBe("integer");
+        });
+        test("rikishi table has a debut column as VARCHAR", async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'rikishi'
+                AND column_name = 'debut';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("debut");
+            expect(column.data_type).toBe("character varying");
+        });
+    });
+    describe('stables_sumos_junc table', () => {
+        test('stables_sumos_junc table exists', async () => {
+                const query = await db.query(
+                    `SELECT EXISTS (
+                        SELECT FROM
+                        information_schema.tables
+                        WHERE
+                        table_name = 'stables_sumos_junc'
+                    );`
+                )
+                const exists = query.rows[0].exists;
+                expect(exists).toBe(true);
+        });
+        test('stables_sumos_junc table contains stable_id from stables table as integer', async () => {
+                const query = await db.query(
+                    `SELECT column_name, data_type
+                    FROM information_schema.columns
+                    WHERE table_name = 'stables_sumos_junc'
+                    AND column_name = 'stable_id';`
+                );
+                const column = query.rows[0]
+                expect(column.column_name).toBe("stable_id");
+                expect(column.data_type).toBe("integer");
+        });
+        test('stables_sumos_junc table contains sumo_id from rikishi table as integer', async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = 'stables_sumos_junc'
+                AND column_name = 'sumo_id';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("sumo_id");
+            expect(column.data_type).toBe("integer");
+        });
+        test('stables_sumos_junc table contains id as serial primary key', async () => {
+            const query = await db.query(
+                `SELECT column_name, data_type, column_default
+                FROM information_schema.columns
+                WHERE table_name = 'stables_sumos_junc'
+                AND column_name = 'id';`
+            );
+            const column = query.rows[0]
+            expect(column.column_name).toBe("id");
+            expect(column.data_type).toBe("integer");
+            expect(column.column_default).toBe(
+                "nextval('stables_sumos_junc_id_seq'::regclass)"
+            )
+    })
+    });
 });
--- db/seed.js
+++ db/seed.js
@@ -3,7 +3,7 @@ const db = require('./connection.js')
 const seed = async ({users, stables, rikishi}) => {
     await db.query(`DROP TABLE IF EXISTS users;`)
     await db.query(`DROP TABLE IF EXISTS stables;`)
-    await db.query(`DROP TABLE IF EXISTS sumos;`)
+    await db.query(`DROP TABLE IF EXISTS rikishi;`)
     await db.query(`DROP TABLE IF EXISTS stables_sumos_junc;`)
     //create stables
     await db.query(`CREATE TABLE stables (
@@ -18,7 +18,29 @@ const seed = async ({users, stables, rikishi}) => {
         user_id SERIAL PRIMARY KEY NOT NULL
     );`)
     //create sumos
+
+    await db.query(`CREATE TABLE rikishi (
+        id SERIAL PRIMARY KEY NOT NULL,
+        sumodb_id INT NOT NULL,
+        nsk_id INT NOT NULL,
+        shikona_en VARCHAR(50),
+        shikona_jp VARCHAR(50),
+        current_rank VARCHAR(100),
+        heya VARCHAR(50),
+        birth_date DATE,
+        shusshin VARCHAR(100),
+        height INT,
+        weight INT,
+        debut VARCHAR(6)
+    );`)
+    
     //create junction table (many to many)
+    
+    await db.query(`CREATE TABLE stables_sumos_junc (
+        stable_id INTEGER REFERENCES stables(stable_id),
+        sumo_id INTEGER REFERENCES rikishi(id),
+        id SERIAL PRIMARY KEY
+    );`)
     //populate stables
     //populate users
     //populate sumos
