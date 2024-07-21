3b778f42862eb66b2b947f55f4322fc85a2bbe41
nc-niamh-smith <niamh.smith@northcoders.com>
2024-07-21 09:25:50 +0100

    ready for hosting

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,18 @@ const { Pool } = require('pg');
 
 const ENV = process.env.NODE_ENV || 'development';
 
+const config = {}
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}`});
 
+if(ENV === 'production') {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE or DATABASE_URL configured!");
 }
 
-const pool = new Pool();
+const pool = new Pool(config);
 
 module.exports = pool;
